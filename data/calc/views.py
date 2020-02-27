edca821d264cff2de0daac31ee41d963036727d1
Nitin Mishra <mnitin2311@gmail.com>
2020-02-27 23:03:29 +0530

    Button

---
+++ data/calc/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html',{'name':'nitin'})
+def add(request):
+    return render(request,'result.html')
